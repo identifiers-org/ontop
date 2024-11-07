648c60ad4c438c2d018a72cf069febc4480a5d2b
Renato Juaçaba Neto <rjuacaba@ebi.ac.uk>
2024-11-07 17:17:03 +0100

    Update script in progress

--- update-triples-script.sh
+++ update-triples-script.sh
@@ -7,12 +7,15 @@ ontop materialize --db-user "$ONTOP_DB_USER" --db-password "$ONTOP_DB_PASSWORD" 
 # if diff -q new-idorg-ontology-materialized.rdf idorg-ontology-materialized.rdf > /dev/null; then
 echo Updating materialized file
 old_sha=$(wget --no-check-certificate -O - -q https://api.github.com/repos/identifiers-org/ontop/contents/idorg-ontology/idorg-ontology-materialized.rdf | grep sha | cut -d\" -f4)
+echo -n "{\"message\":\"Sync materialized registry tuples with database\", \"committer\":{\"name\":\"Ontop bot\", \"email\":\"identifiers-org@ebi.ac.uk\"}, \"content\":\"" > datafile
+cat new-idorg-ontology-materialized.rdf|base64 >> datafile
+echo "\", \"sha\": \"$old_sha\"}" >> datafile
 wget --method=PUT --no-check-certificate \
      -H "Accept: application/vnd.github+json"\
      -H "Authorization: Bearer $ONTOP_GH_TOKEN"\
      -H "X-GitHub-Api-Version: 2022-11-28"\
-     --body-data="{\"message\":\"Sync materialized registry tuples with database\", \"committer\":{\"name\":\"Ontop bot\", \"email\":\"identifiers-org@ebi.ac.uk\"}, \"content\":\"$(cat new-idorg-ontology-materialized.rdf|base64)\", \"sha\": \"$old_sha\"}"\
-     https://api.github.com/repos/identifiers-org/ontop/contents/idorg-ontology/idorg-ontology-materialized.rdf\
+     --body-file=datafile \
+     https://api.github.com/repos/identifiers-org/ontop/contents/idorg-ontology/idorg-ontology-materialized.rdf
 # else
 #   echo No need to update materialized file
 # fi
