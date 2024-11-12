74a3359c4d97220724ec2a8470690710d66531f9
Renato Juaçaba Neto <rjuacaba@ebi.ac.uk>
2024-11-12 11:07:22 +0000

    Working on daily updates

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 # Seems like the ontop image does not ship with the postgres jdbc driver
-#  This docker file is simply to create a image for adding that
+#  This docker file is mostly to create a image for adding that
+#  cURL is also installed to enable the daily updates of the github repo
 
 FROM ontop/ontop:5.1.2
 USER "root"
-RUN ["wget", "-q", "--no-check-certificate", "https://jdbc.postgresql.org/download/postgresql-42.7.4.jar", "-O", "/tmp/postgres-jdbc-driver.jar"]
-RUN ["cp", "/tmp/postgres-jdbc-driver.jar", "/opt/ontop/lib/"]
+
+RUN apt-get -qq update && apt-get -qq install -y curl && rm -rf /var/lib/apt/lists/*
+
+RUN wget -q --no-check-certificate https://jdbc.postgresql.org/download/postgresql-42.7.4.jar -O /opt/ontop/lib/postgres-jdbc-driver.jar
--- update-triples-script.sh
+++ update-triples-script.sh
@@ -6,15 +6,17 @@ wget --no-check-certificate -q https://raw.githubusercontent.com/identifiers-org
 ontop materialize --db-user "$ONTOP_DB_USER" --db-password "$ONTOP_DB_PASSWORD" --db-url "$ONTOP_DB_URL" -m ./idorg-ontology.obda -t ./idorg-ontology.ttl -o ./new-idorg-ontology-materialized.rdf -p /dev/null
 # if diff -q new-idorg-ontology-materialized.rdf idorg-ontology-materialized.rdf > /dev/null; then
 echo Updating materialized file
-old_sha=$(wget --no-check-certificate -O - -q https://api.github.com/repos/identifiers-org/ontop/contents/idorg-ontology/idorg-ontology-materialized.rdf | grep sha | cut -d\" -f4)
+
 echo -n "{\"message\":\"Sync materialized registry tuples with database\", \"committer\":{\"name\":\"Ontop bot\", \"email\":\"identifiers-org@ebi.ac.uk\"}, \"content\":\"" > datafile
 cat new-idorg-ontology-materialized.rdf|base64 >> datafile
+old_sha=$(wget --no-check-certificate -O - -q https://api.github.com/repos/identifiers-org/ontop/contents/idorg-ontology/idorg-ontology-materialized.rdf | grep sha | cut -d\" -f4)
 echo "\", \"sha\": \"$old_sha\"}" >> datafile
-wget --method=PUT --no-check-certificate \
-     -H "Accept: application/vnd.github+json"\
-     -H "Authorization: Bearer $ONTOP_GH_TOKEN"\
-     -H "X-GitHub-Api-Version: 2022-11-28"\
-     --body-file=datafile \
+
+curl -X PUT --insecure \
+     -H "Accept: application/vnd.github+json" \
+     -H "Authorization: Bearer $ONTOP_GH_TOKEN" \
+     -H "X-GitHub-Api-Version: 2022-11-28" \
+     --data=@datafile \
      https://api.github.com/repos/identifiers-org/ontop/contents/idorg-ontology/idorg-ontology-materialized.rdf
 # else
 #   echo No need to update materialized file
